929e7162a9d9bb538be56526fbbc320ad3ba15bd
Sonja <sonjakrivokapic00@gmail.com>
2023-05-06 12:30:23 +0200

    add

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-// isArray() method is used to check if an object is an array. The Array. isArray() method returns true if an object is an array, otherwise returns false . Note: For an array, the typeof operator returns an object.
-
-function playRound(playerSelection, computerSelection){
-    
-    if(playerSelection == "scissors" && computerSelection == "scissors"){
-        return "equal"
-    }
-    if(playerSelection == "scissors" && computerSelection == "rock"){
-        return `The winner is ${computerSelection}!` 
-    }
-    if(playerSelection == "scissors" && computerSelection == "paper"){
-        return `The winner is ${playerSelection}!`  
-    }
-    if(playerSelection == "rock" && computerSelection == "scissors"){
-        return `The winner is ${computerSelection}!` 
-    }
-    if(playerSelection == "rock" && computerSelection == "rock"){
-        return "equal"
-    }
-    if(playerSelection == "rock" && computerSelection == "paper"){
-        return `The winner is ${playerSelection}!`  
-    }
-    if(playerSelection == "paper" && computerSelection == "scissors"){
-        return `The winner is ${computerSelection}!`  
-    }
-    if(playerSelection == "paper" && computerSelection == "rock"){
-        return `The winner is ${computerSelection}!`  
-    }
-    if(playerSelection == "paper" && computerSelection == "paper"){
-        return "equal"  
-    }
-}
-
-    function getComputerChoice(){
-        let niz1 = ['rock', 'paper', 'scissors']
-        let choice;
-        let random = Math.floor(Math.random() * 3)
+function getComputerChoice(){
+    let niz1 = ['rock', 'paper', 'scissors']
+    let choice;
+    let random = Math.floor(Math.random() * 3)
         for(let i = 0; i < niz1.length; i++){
             if(i == random){
                 choice = niz1[i] 
@@ -42,11 +9,89 @@ function playRound(playerSelection, computerSelection){
         }
         return choice
     };
-
-
+        
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection == "scissors" && computerSelection == "scissors"){
+            return "equal"
+        }
+        
+        if(playerSelection == "scissors" && computerSelection == "rock"){
+            return "The winner is Computer!"
+        }
+        
+        if(playerSelection == "scissors" && computerSelection == "paper"){
+            return "The winner is Player!"  
+        }
+        
+        if(playerSelection == "rock" && computerSelection == "scissors"){
+            return "The winner is Player!"
+        }   
+        
+        if(playerSelection == "rock" && computerSelection == "rock"){
+            return "equal"
+        }
+        
+        if(playerSelection == "rock" && computerSelection == "paper"){
+            return `The winner is Computer!`  
+        }
+        
+        if(playerSelection == "paper" && computerSelection == "scissors"){
+            return `The winner is Computer!`  
+        }
+        
+        if(playerSelection == "paper" && computerSelection == "rock"){
+            return `The winner is Computer!`  
+        }
+        
+        if(playerSelection == "paper" && computerSelection == "paper"){
+            return "equal"  
+        }
+    }
     
-    console.log(playRound("rock", getComputerChoice()));
+    function game(){
+        
+        let res = '';
+        let computer = 0;
+        let player = 0;
+        let nereseno = 0;
+        let pobednik;
 
+        for (let i = 0; i < 3; i++){
 
+            const playerSelection = prompt();
+            const computerSelection = getComputerChoice();
+            res = playRound(playerSelection, computerSelection)
+            
+            if(res === `The winner is Computer!`){
+                computer += 1
+            }
+            if(res === "The winner is Player!"){
+                player += 1 
+            }
+            else if(res === "equal" ){
+                nereseno += 1
+            }
+            pobednik = (computer > player) ?
+            (computer > nereseno ? computer : nereseno) :
+            (player > nereseno ? player : nereseno);    
+        }
+        
+       let ispisi;
+        if(pobednik == computer){
+            ispisi = `The winner is Computer!`
+        }
+        if(pobednik == player){
+            ispisi = "The winner is Player!"
+        }
+        if(pobednik == nereseno){
+            ispisi =  "equal"
+        }
+        return ispisi
+    }
+
+    console.log(game())
+    
+
+  
 
 
